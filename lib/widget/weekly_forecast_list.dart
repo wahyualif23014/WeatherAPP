10494e9729b890f0d335ce38202062d280961580
wahyualif23014 <zexvulca@gmail.com>
2025-06-15 20:56:48 +0700

    new glassmorpishm weekley forcasst

--- lib/widget/weekly_forecast_list.dart
+++ lib/widget/weekly_forecast_list.dart
@@ -1,5 +1,6 @@
 // widgets/weekly_forecast_list.dart
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class WeeklyForecastList extends StatelessWidget {
   const WeeklyForecastList({super.key});
@@ -17,54 +18,101 @@ class WeeklyForecastList extends StatelessWidget {
     ];
 
     return SizedBox(
-      height: 140,
+      height: 160,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: forecastData.length,
         itemBuilder: (context, index) {
           final data = forecastData[index];
-          return Container(
-            width: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple[700],
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 6,
-                  offset: Offset(2, 4),
-                ),
-              ],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  data["day"],
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white70,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Icon(
-                  data["icon"],
-                  color: Colors.white,
-                  size: 30,
-                ),
-                Text(
-                  "${data["max"]}° / ${data["min"]}°",
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            child: ForecastItem(data: data),
           );
         },
+      ),
+    );
+  }
+}
+class ForecastItem extends StatefulWidget{
+  final Map<String, dynamic> data;
+
+  const ForecastItem({super.key, required this.data});
+  @override
+  State<ForecastItem> createState() => _ForecastItemState();
+}
+class _ForecastItemState extends State<ForecastItem>{
+  double _scale =1.0;
+  bool _ispressed = false;
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() {
+      _ispressed = true;
+      _scale = 0.9;
+    });
+  }
+   void _onTapUp(TapUpDetails details) {
+    setState(() {
+      _ispressed = false;
+      _scale = 1.0;
+    });
+  }
+  void _onTapCancel() {
+    setState(() {
+      _ispressed = false;
+      _scale = 1.0;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: Transform.scale(
+        scale: _scale,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.white.withOpacity(0.2)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blueGrey.withOpacity(0.2),
+                blurRadius: 8,
+                offset: const Offset(0, 4),
+              )
+            ],
+            backgroundBlendMode: BlendMode.multiply,
+          ),
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                widget.data["day"],
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(
+                widget.data["icon"],
+                color: Colors.white,
+                size: 30,
+              ),
+              Text(
+                "${widget.data["max"]}° / ${widget.data["min"]}°",
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
