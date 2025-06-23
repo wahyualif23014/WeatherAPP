384ce3893529df1ce33f8454943e3736068ee913
wahyualif23014 <zexvulca@gmail.com>
2025-06-23 20:14:30 +0700

    new 23 chaos

--- lib/theme/appcolor.dart
+++ lib/theme/appcolor.dart
@@ -3,4 +3,5 @@ class AppColors {
   static const primary = Color(0xFF2E2961);
   static const primaryLight = Color(0xFF5E59A6);
   static const primaryDark = Color(0xFF1C1939);
+  static const light = Color(0xFFF5F5F5);
 }
