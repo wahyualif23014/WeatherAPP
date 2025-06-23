0683819479c5809ee674731f898237a01210572d
wahyualif23014 <zexvulca@gmail.com>
2025-06-23 20:15:00 +0700

    new 23 chaos

--- lib/theme/appcolor.dart
+++ lib/theme/appcolor.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const primaryDark = Color(0xFF1C1939);
   static const light = Color(0xFFF5F5F5);
   static const dark = Color(0xFF121212);
+  static const accent = Color(0xFF4CAF50);
 }
